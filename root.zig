1085490859826df4f3c61dee5edbddbbc824bf10
Antoine <antoine.gagniere@orolia2s.com>
2025-05-18 22:13:08 +0200

    Add another skybox: constellations boundaries

--- root.zig
+++ root.zig
@@ -2,4 +2,5 @@ pub const skybox = struct {
     pub const milkyway = @embedFile("milkyway/milkyway_4k.jpg");
     pub const constellations = @embedFile("milkyway/constellations_4k.jpg");
     pub const stars = @embedFile("milkyway/stars_4k.jpg");
+    pub const boundaries = @embedFile("milkyway/boundaries_4k.jpg");
 };
